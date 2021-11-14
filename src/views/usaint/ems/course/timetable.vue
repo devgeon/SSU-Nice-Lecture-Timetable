7cc6508d92d1b96be4b6f7c23b4bd24d90e41ec1
kangsr <seryun8096@gmail.com>
2021-11-14 22:55:09 +0900

    images added

--- src/views/usaint/ems/course/timetable.vue
+++ src/views/usaint/ems/course/timetable.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>Hello world</div>
+  
+  <div>
+   
+    <div class="semester">
+      <p>학년도:</p>
+
+      <p>학기:</p>
+
+      <button>이전학기</button>
+      <button>다음학기</button>
+      
+      <div class="menu">
+        <a>학부전공별</a>
+        <a>교양필수</a>
+        <a>교양선택</a>
+        <a>연계전공</a>
+        <a>융합전공</a>
+        <a>대학원</a>
+        <a>타전공인정과목</a>
+        <a>과목검색</a>
+      </div>
+      
+      <div class="filter">
+        <a><img src="./src/assets/images/timetable/yellow.png"></a>
+      </div>
+    </div>
+
+
+
+
+
+
+  </div>
+
+
 </template>

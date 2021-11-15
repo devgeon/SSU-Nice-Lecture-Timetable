a7ba985e4cf67f83e4302bc2982d3c5e86b205e4
kangsr <seryun8096@gmail.com>
2021-11-15 16:10:06 +0900

    11/15

--- src/views/usaint/ems/course/timetable.vue
+++ src/views/usaint/ems/course/timetable.vue
@@ -11,27 +11,27 @@
       <button>다음학기</button>
       
       <div class="menu">
-        <a>학부전공별</a>
-        <a>교양필수</a>
-        <a>교양선택</a>
-        <a>연계전공</a>
-        <a>융합전공</a>
-        <a>대학원</a>
-        <a>타전공인정과목</a>
-        <a>과목검색</a>
+        <a id="m1">학부전공별</a>
+        <a id="m2">교양필수</a>
+        <a id="m3">교양선택</a>
+        <a id="m4">연계전공</a>
+        <a id="m5">융합전공</a>
+        <a id="m6">대학원</a>
+        <a id="m7">타전공인정과목</a>
+        <a id="m8">과목검색</a>
       </div>
       
+
       <div class="filter">
-        
+        <button type="button"><img src="@/assets/images/timetable/yellow.png"/></button>
+        <button type="button"><img src="@/assets/images/timetable/red.png"/></button>
+        <button type="button" disabled><img src="@/assets/images/timetable/blue.png"/></button>
       </div>
     </div>
 
 
-
-
-
-
   </div>
 
-
 </template>
+
+

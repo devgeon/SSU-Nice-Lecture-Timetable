4f94c0b9e4a78d2b0282defc5c117e4c751de5e7
mathpaul3 <mathpaul3@gmail.com>
2021-11-22 00:29:02 +0900

    modify imports

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import App from './App.vue'
 import router from './router'
 
 //////////bootstrapvue
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Multi Language
 import VueI18n from 'vue-i18n'

196f8247e3932f7ce6174480dbdfa98c9219857a
kangsr <seryun8096@gmail.com>
2021-12-01 23:17:39 +0900

    used <table>tag

--- src/components/CourseTable.vue
+++ src/components/CourseTable.vue
@@ -16,7 +16,7 @@
             
             <slot></slot>
             
-            <div id="table">
+            <!-- <div id="table">
                 <div
                     class="info"
                     style="
@@ -29,7 +29,7 @@
                 >
                     <p id="info1">과목번호</p>
                     <p id="info2">이수구분</p>
-                    <!-- <p id="info3">다전공</p> -->
+                   
                     <p id="info4">과목명</p>
                     <p id="info5">교수명</p>
                     <p id="info6">강의시간</p>
@@ -40,7 +40,7 @@
                     <p id="info11">교과영역</p>
                     <p id="info12">비고</p>
                 </div>
-                <!-- overflow: auto -->
+               
                 <div
                     style="height: 550px; overflow: auto; width: 80%; display: block"
                     id="table"
@@ -69,7 +69,6 @@
                                 />
                             </span>
                         </p>
-                        <!-- <p id="course_sub"></p> -->
                         <p id="name">{{ course.subject_name }}</p>
                         <p id="professor">{{ course.professors }}</p>
                         <div
@@ -98,8 +97,82 @@
                         <p id="etc">{비고}</p>
                     </div>
                 </div>
-            </div>
-        </div>
+            </div>-->
+        </div> 
+
+        <table >
+            <thead>
+                <tr class = "header" style="background: #e4e3e3; width:80%; position:sticky; top:0;">
+                    <th>과목번호</th>
+                    <th>이수구분</th>
+                    <th>과목명</th>
+                    <th>교수명</th>
+                    <th>강의시간</th>
+                    <th>강의실</th>
+                    <th>학점(설계)</th>
+                    <th>여석/수강인원</th>
+                    <th>공학인증</th>
+                    <th>교과영역</th>
+                    <th>비고</th>
+                </tr>
+            </thead>
+
+            <tbody style="height: 550px; overflow: auto; width: 80%; display: block">
+                <div
+                    :id="course.idx % 2 == 0 ? 'greyBox' : 'whiteBox'"
+                    v-for="course of courses"
+                    :key="course.idx"
+                >
+                    <tr>
+                        <td rowspan="2" id="number">{{ course.number }}</td>
+
+                        <td rowspan="2" id="course_main"
+                            v-for="course_type of course.course_types"
+                            :key="'ct' + course_type.idx"
+                        >
+                            <span class="hoverImg">
+                                <img
+                                    id="iconMain"
+                                    :src="
+                                        '/assets/images/timetable/' +
+                                        course_type.type[0] +
+                                        course_type.type[1] +
+                                        '.png'
+                                    "
+                                    alt=""
+                                />
+                            </span>
+                        </td>
+
+                        <td rowspan="2" id="name">{{ course.subject_name }}</td>
+                        <td rowspan="2" id="professor">{{ course.professors }}</td>
+                        
+                        <div
+                            id="timetables"
+                            v-for="timetable of course.timetables"
+                            :key="timetable.idx"
+                            
+                        >
+                            <td rowspan="1" :id="'time' + timetable.idx" >
+                                {{
+                                    timetable.day + " " + timetable.start + "~" + timetable.end
+                                }}
+                            </td>
+                            <td rowspan="1" :id="'loc' + timetable.idx">{{ timetable.loc.location }}</td>
+                        </div>
+
+                        <td rowspan="2" id="credit">
+                            {{ course.credit + " (" + course.credit_planned + ")" }}
+                        </td>
+                        <td rowspan="2" id="seats">{{ 0 }}</td>
+                        <td rowspan="2" id="certification">{{ course.certifications }}</td>
+                        <td rowspan="2" id="subject_area">{교과영역}</td>
+                        <td rowspan="2" id="etc">{비고}</td>
+                    </tr>
+                </div>
+
+            </tbody>
+        </table>
     </div>
 </template>
 

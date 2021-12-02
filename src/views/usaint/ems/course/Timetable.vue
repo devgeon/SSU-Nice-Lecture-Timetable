a640511e2ae11c06cfa1c0f0d9da3ce62bcfd5d7
kangsr <seryun8096@gmail.com>
2021-12-03 01:10:39 +0900

    better semester select area

--- src/views/usaint/ems/course/Timetable.vue
+++ src/views/usaint/ems/course/Timetable.vue
@@ -3,33 +3,48 @@
     <div
       style="position: sticky; top: 0; background-color: #ffffff; z-index: 1"
     >
-      <div class="semester">
-        <p>학년도:</p>
+      <div class="semester" style="margin-top:30px; margin-left:40px;" >
+        <p class="selectYear" style="float:left; margin-top:10px; margin-right:10px;">학년도:</p>
 
-        <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
-          <b-dropdown-item>First Action</b-dropdown-item>
-          <b-dropdown-item>Second Action</b-dropdown-item>
-          <b-dropdown-item>Third Action</b-dropdown-item>
-          <b-dropdown-divider></b-dropdown-divider>
-          <b-dropdown-item active>Active action</b-dropdown-item>
-          <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+        <b-dropdown id="dropdown-1" text="2021" class="m-md-2" style="width:100px;">
+          <div style="overflow:auto; height:100px;">
+            <b-dropdown-item>2010</b-dropdown-item>
+            <b-dropdown-item>2011</b-dropdown-item>
+            <b-dropdown-item>2012</b-dropdown-item>
+            <b-dropdown-item>2013</b-dropdown-item>
+            <b-dropdown-item>2014</b-dropdown-item>
+            <b-dropdown-item>2015</b-dropdown-item>
+            <b-dropdown-item>2016</b-dropdown-item>
+            <b-dropdown-item>2017</b-dropdown-item>
+            <b-dropdown-item>2018</b-dropdown-item>
+            <b-dropdown-item>2019</b-dropdown-item>
+            <b-dropdown-item>2020</b-dropdown-item>
+
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item active>2021</b-dropdown-item>
+            <b-dropdown-item>2022</b-dropdown-item>
+            <!-- <b-dropdown-item disabled>Disabled action</b-dropdown-item> -->
+          </div>
         </b-dropdown>
 
-        <p>학기:</p>
-        <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
-          <b-dropdown-item>First Action</b-dropdown-item>
-          <b-dropdown-item>Second Action</b-dropdown-item>
-          <b-dropdown-item>Third Action</b-dropdown-item>
+        <p  class="selectSemester" style="float:left; margin-top:10px; margin-right:10px;">학기:</p>
+
+        <b-dropdown id="dropdown-1" text="1학기" class="m-md-2" style="width:100px;">
+
           <b-dropdown-divider></b-dropdown-divider>
-          <b-dropdown-item active>Active action</b-dropdown-item>
-          <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+          <b-dropdown-item active>1학기</b-dropdown-item>
+          <b-dropdown-item>여름학기</b-dropdown-item>
+          <b-dropdown-item>2학기</b-dropdown-item>
+          <b-dropdown-item>겨울학기</b-dropdown-item>
+
+
         </b-dropdown>
-        <button>이전학기</button>
-        <button>다음학기</button>
+        <button style="margin-top:6px; margin-left:10px;">이전학기</button>
+        <button style="margin-top:6px;">다음학기</button>
       </div>
 
       <div>
-        <b-tabs content-class="mt-3" style="margin-top: 30px">
+        <b-tabs content-class="mt-3" style="margin-top: 30px; margin-left:40px;">
           <b-tab
             @click="$router.push({ name: 'hakbu' })"
             title="학부전공별"

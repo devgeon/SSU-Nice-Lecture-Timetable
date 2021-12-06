bda5fade2ec75f680124f90d815a9f73738001ec
DWL21 <76774809+DWL21@users.noreply.github.com>
2021-12-06 21:10:51 +0900

    nav style 수정, 교필 nav삭제

--- src/views/usaint/ems/course/Kyopill.vue
+++ src/views/usaint/ems/course/Kyopill.vue
@@ -1,26 +1,4 @@
-<template>
-  <CourseTable v-bind:axios_params="axios_params">
-    <div class="leftNav" style="width: 150px; position: sticky; top: 0">
-      <nav class="dm-menu">
-        <ul class="dm-menu__list">
-          <li class="dm-menu__item dm-menu__item--hasMore" tabindex="0">
-            <a class="dm-menu__itemLink">교필 1단계</a>
-            <ul class="dm-menu__list">
-              <li class="dm-menu__item dm-menu__item--hasMore" tabindex="0">
-                <a class="dm-menu__itemLink">교필 2단계</a>
-                <ul class="dm-menu__list">
-                  <li class="dm-menu__item" tabindex="0">
-                    <a class="dm-menu__itemLink">교필 3단계</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </CourseTable>
-</template>
+
 
 <script>
 export default {
